1c0a7a7ef75b5de3f468f5fd9c0505e3b0e95924
Guillaume De win <gde-win@fu-r6-p2.s19.be>
2024-04-13 16:51:01 +0200

    using __func__ macro

--- src/philo.c
+++ src/philo.c
@@ -6,16 +6,16 @@
 /*   By: gde-win <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 22:42:34 by gde-win           #+#    #+#             */
-/*   Updated: 2024/04/13 16:42:52 by gde-win          ###   ########.fr       */
+/*   Updated: 2024/04/13 16:50:11 by gde-win          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	ft_exit(char *error_message)
+void	ft_exit(char *caller_name, char *error_message)
 {
 	if (error_message)
-		printf("%s%s%s\n", BOLD_BLUE, error_message, END_COLOR);
+		printf("%s%s: %s%s\n", BOLD_BLUE, caller_name, error_message, END_COLOR);
 	exit(EXIT_SUCCESS);
 }
 
@@ -27,21 +27,21 @@ static void	ft_check_args(int ac, char **av, int *numeric_args)
 	int		result;
 
 	if (ac != 5 && ac != 6)
-		ft_exit(USAGE);
+		ft_exit((char *)__func__, USAGE);
 	numeric_args[5] = -1;
 	while (--ac > 0)
 	{
 		num = ft_atoi(av[ac]);
 		copy = ft_itoa(num);
 		if (!copy)
-			ft_exit(MALLOC_FAIL);
+			ft_exit((char *)__func__, MALLOC_FAIL);
 		offset = false;
 		if (av[ac][0] == '+' || av[ac][0] == '-')
 			offset = true;
 		result = ft_strcmp(av[ac] + offset, copy);
 		free(copy);
 		if (result != 0/* || num == 0*/)
-			ft_exit(INVALID_ARGS);
+			ft_exit((char *)__func__, INVALID_ARGS);
 		numeric_args[ac] = num;
 	}
 }
@@ -51,11 +51,10 @@ int	main(int ac, char **av)
 	int			numeric_args[6];
 	pthread_t	*philosophers;
 
-	printf("%s\n", __func__);
 	ft_check_args(ac, av, numeric_args);
 	philosophers = (pthread_t *)malloc(numeric_args[NUMBER_OF_PHILOSOPHERS] * sizeof(pthread_t));
 	if (!philosophers)
-		ft_exit(MALLOC_FAIL);
+		ft_exit((char *)__func__, MALLOC_FAIL);
 	(void)av;
 	return (0);
 }
